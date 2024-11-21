24872e60daf677ba370cb779bfc895142c431d6c
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-21 08:50:24 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/templates/static/src/index.js
+++ threagile-monitoring/src/threagile_monitoring/templates/static/src/index.js
@@ -7,7 +7,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./style.css";
+import "./main.css";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
