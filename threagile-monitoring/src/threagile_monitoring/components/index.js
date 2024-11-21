f37ed30a80928ee7336e8f43a9e02918e5fa0a35
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-21 11:52:41 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/components/index.js
+++ threagile-monitoring/src/threagile_monitoring/components/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import Dashboard from "./Dashboard";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>
 );
