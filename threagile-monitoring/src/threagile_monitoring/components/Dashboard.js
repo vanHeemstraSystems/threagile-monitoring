9131ff1708d30bfec98efb25ec96fe35e6feadbf
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-21 14:11:10 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/components/Dashboard.js
+++ threagile-monitoring/src/threagile_monitoring/components/Dashboard.js
@@ -1617,6 +1617,51 @@ const Dashboard = ({ risksJson }) => {
   }, [risksJson])
 
   if (!data) return <div>Loading...</div>
+
+  // Custom Active Shape for Pie Chart
+  const renderActiveShape = props => {
+    const {
+      cx,
+      cy,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      payload,
+      percent,
+      value
+    } = props
+
+    return (
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor='middle' fill={fill}>
+          {payload.name}
+        </text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+      </g>
+    )
+  }
+
+  // MORE
+  
 }
 
 export default Dashboard
