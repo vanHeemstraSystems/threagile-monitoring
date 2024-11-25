75a8dea69658b7bf4b863a7ec9729e0ddefc1d89
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-25 12:19:46 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/components/Dashboard.js
+++ threagile-monitoring/src/threagile_monitoring/components/Dashboard.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import { ScatterChart, Scatter, XAxis, 
-    YAxis, CartesianGrid } from 'recharts';
+import React, { useState, useEffect } from 'react'
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid } from 'recharts'
+import { AlertCircle, Activity, TrendingDown, Settings } from 'lucide-react'
 
 const Dashboard = ({ risksJson }) => {
- 
   // Sample data
   const data = [
-      { x: 1, y: 23 },
-      { x: 2, y: 3 },
-      { x: 3, y: 15 },
-      { x: 4, y: 35 },
-      { x: 5, y: 45 },
-      { x: 6, y: 25 },
-      { x: 7, y: 17 },
-      { x: 8, y: 32 },
-      { x: 9, y: 43 },
-  ];
+    { x: 1, y: 23 },
+    { x: 2, y: 3 },
+    { x: 3, y: 15 },
+    { x: 4, y: 35 },
+    { x: 5, y: 45 },
+    { x: 6, y: 25 },
+    { x: 7, y: 17 },
+    { x: 8, y: 32 },
+    { x: 9, y: 43 }
+  ]
 
   return (
-      <ScatterChart width={400} height={400}>
-          <CartesianGrid />
-          <XAxis type="number" dataKey="x" />
-          <YAxis type="number" dataKey="y" />
-          <Scatter data={data} fill="green" />
-      </ScatterChart>
-  );
+    <ScatterChart width={400} height={400}>
+      <CartesianGrid />
+      <XAxis type='number' dataKey='x' />
+      <YAxis type='number' dataKey='y' />
+      <Scatter data={data} fill='green' />
+    </ScatterChart>
+  )
 }
 
-export default Dashboard;
+export default Dashboard
