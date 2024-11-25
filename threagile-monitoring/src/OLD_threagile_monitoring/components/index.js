bf3ec2dd346a49588a0ac4b4203533c10bef8cdf
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-25 10:16:35 +0000

    Update

--- threagile-monitoring/src/OLD_threagile_monitoring/components/index.js
+++ threagile-monitoring/src/OLD_threagile_monitoring/components/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
-// import App from "./App";
+import ReactDOM from 'react-dom/client'
 import Dashboard from './Dashboard'
+import reportWebVitals from './reportWebVitals'
+
 const rootElement = document.getElementById('root')
-const root = createRoot(rootElement)
+const root = ReactDOM.createRoot(rootElement)
 
 // Temporarily hard coded, make dynamic import
 const risksJson = [
@@ -1536,3 +1537,8 @@ root.render(
     <Dashboard risksJson={risksJson} />
   </React.StrictMode>
 )
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals()
