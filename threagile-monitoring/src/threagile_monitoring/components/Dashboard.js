ad35a9039cb99ae3072efab175e2a3cd7eca660f
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-25 15:19:44 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/components/Dashboard.js
+++ threagile-monitoring/src/threagile_monitoring/components/Dashboard.js
@@ -295,16 +295,7 @@ const Dashboard = ({ risksJson }) => {
         {/* Risk Matrix */}
         <div className='bg-white p-4 rounded-lg shadow col-span-2'>
           <h3 className='text-lg font-semibold mb-4'>Risk Matrix</h3>
-          <RiskMatrix
-            data={[
-              { x: 100, y: 200, z: 200 },
-              { x: 120, y: 100, z: 260 },
-              { x: 170, y: 300, z: 400 },
-              { x: 140, y: 250, z: 280 },
-              { x: 150, y: 400, z: 500 },
-              { x: 110, y: 280, z: 200 }
-            ]}
-          />
+          <RiskMatrix data={data} />
           <p className='mt-2 text-sm text-gray-600'>
             Interactive risk matrix showing impact vs likelihood. Click on
             points to see risk details.
