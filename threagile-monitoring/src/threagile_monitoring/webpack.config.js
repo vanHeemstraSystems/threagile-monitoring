bd233700667cde2cb4d8762c8102433b04b9fd45
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-25 12:51:11 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/webpack.config.js
+++ threagile-monitoring/src/threagile_monitoring/webpack.config.js
@@ -1,20 +1,21 @@
-const path = require("path");
+const path = require('path')
 
-module.exports = {
-    entry: "./components/index.js",
-    output: {
-        path: path.join(__dirname, "../threagile_monitoring/static/js"),
-        filename: "app.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
-}
+module.exports = [].map(devtool => ({
+  entry: './components/index.js',
+  output: {
+    path: path.join(__dirname, '../threagile_monitoring/static/js'),
+    filename: 'app.js'
+  },
+  devtool,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}))
