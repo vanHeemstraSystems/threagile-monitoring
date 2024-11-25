2b8fe3dcd8674c471d5c3e89434a5f5931d5dc1f
Willem van Heemstra <wvanheemstra@icloud.com>
2024-11-25 11:17:00 +0000

    Update

---
+++ threagile-monitoring/src/threagile_monitoring/config-overrides.js
@@ -0,0 +1,11 @@
+const paths = require('react-scripts/config/paths')
+const path = require('path')
+
+// Make the "components" folder be treated as the "src" folder
+paths.appSrc = path.resolve(__dirname, 'components')
+// Tell the app that "src/index.js" has moved to "components/index.js"
+paths.appIndexJs = path.resolve(__dirname, 'components/index.js')
+// Make the "static" folder be treated as the "public" folder
+paths.appPublic = path.resolve(__dirname, 'static')
+// Tell the app that "src/index.html" has moved to "templates/index.html"
+paths.appHtml = path.resolve(__dirname, 'templates/index.html')
