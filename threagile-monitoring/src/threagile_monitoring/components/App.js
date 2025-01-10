34e73014ca4ca92e349f872d2c72c8259657d5b7
Willem van Heemstra <wvanheemstra@icloud.com>
2025-01-10 14:51:46 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/components/App.js
+++ threagile-monitoring/src/threagile_monitoring/components/App.js
@@ -1,29 +1,4 @@
 import { React, Router, Route, Switch } from 'react'
-// import { ScatterChart, Scatter, XAxis,
-//     YAxis, CartesianGrid } from 'recharts';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App () {
   return (
@@ -41,30 +16,5 @@ function App () {
     </div>
   )
 }
-
-// const App = () => {
-
-//   // Sample data
-//   const data = [
-//       { x: 1, y: 23 },
-//       { x: 2, y: 3 },
-//       { x: 3, y: 15 },
-//       { x: 4, y: 35 },
-//       { x: 5, y: 45 },
-//       { x: 6, y: 25 },
-//       { x: 7, y: 17 },
-//       { x: 8, y: 32 },
-//       { x: 9, y: 43 },
-//   ];
-
-//   return (
-//       <ScatterChart width={400} height={400}>
-//           <CartesianGrid />
-//           <XAxis type="number" dataKey="x" />
-//           <YAxis type="number" dataKey="y" />
-//           <Scatter data={data} fill="green" />
-//       </ScatterChart>
-//   );
-// }
 
 export default App
