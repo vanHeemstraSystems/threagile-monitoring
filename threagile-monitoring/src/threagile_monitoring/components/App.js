37ab5b0c5430f6e25d980edff1953303a31713cd
Willem van Heemstra <wvanheemstra@icloud.com>
2025-01-10 16:18:55 +0000

    Update

--- threagile-monitoring/src/threagile_monitoring/components/App.js
+++ threagile-monitoring/src/threagile_monitoring/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { Route, Routes } from 'react-router'
 import Layout from './Layout'
 import NoMatch from './NoMatch'
+
+// In react-router-dom v6, "Switch" is replaced by routes "Routes". You need to update the import from
+
+// import { Switch, Route } from "react-router-dom";
+// to
+// import { Routes ,Route } from 'react-router-dom';
+
+// You also need to update the Route declaration from
+
+// <Route path="/" component={Home} />
+// to
+// <Route path='/welcome' element={<Home/>} />
+  
+// In react-router-dom, you also do not need to use the exact in the Route declaration.
+
+// For more information, you can visit the official documentation:
+// https://reactrouter.com/docs/en/v6/upgrading/v5
 
 const App = () => {
   return (
     <>
-      <Switch>
+      <Routes>
         <Route exact path='/'>
           <Layout showHome />
         </Route>
@@ -16,7 +33,7 @@ const App = () => {
         <Route path='*'>
           <NoMatch />
         </Route>
-      </Switch>
+      </Routes>
     </>
   )
 }
