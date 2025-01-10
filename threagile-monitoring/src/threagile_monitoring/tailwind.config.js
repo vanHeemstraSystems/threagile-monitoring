3b310d4758b4bab93cc052f33dbbb11400ca2763
Willem van Heemstra <wvanheemstra@icloud.com>
2025-01-10 11:08:50 +0000

    Update TailwindCSS config

--- threagile-monitoring/src/threagile_monitoring/tailwind.config.js
+++ threagile-monitoring/src/threagile_monitoring/tailwind.config.js
@@ -1,17 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./app/templates/**/*.html",
-        "./app/static/**/*.{css,js}",
-        "./node_modules/flowbite/**/*.js"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        require('flowbite/plugin')({
-            datatables: true,
-        }),
-        // ... other plugins
-    ],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    './app/templates/**/*.html',
+    './app/static/**/*.{css,js}',
+    './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('flowbite/plugin')({
+      datatables: true
+    })
+    // ... other plugins
+  ],
+
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B'
+        }
+      }
+    }
+  }
 }
