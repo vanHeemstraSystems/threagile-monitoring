1a9cf9bc61bb351e99d3802742c0880bddd4c4ec
Willem van Heemstra <wvanheemstra@icloud.com>
2025-01-16 14:24:57 +0100

    refactor: Update routing to use react-router-dom v6, replace react-router, and clean up App component structure; adjust package dependencies and manifest paths for consistency

--- threagile-monitoring/src/threagile_monitoring/components/App.js
+++ threagile-monitoring/src/threagile_monitoring/components/App.js
@@ -1,38 +1,15 @@
 import React from 'react'
-import { Route, Routes } from 'react-router'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
 import NoMatch from './NoMatch'
-
-// In react-router-dom v6, "Switch" is replaced by routes "Routes". You need to update the import from
-
-// import { Switch, Route } from "react-router-dom";
-// to
-// import { Routes ,Route } from 'react-router-dom';
-
-// You also need to update the Route declaration from
-
-// <Route path="/" component={Home} />
-// to
-// <Route path='/welcome' element={<Home/>} />
-  
-// In react-router-dom, you also do not need to use the exact in the Route declaration.
-
-// For more information, you can visit the official documentation:
-// https://reactrouter.com/docs/en/v6/upgrading/v5
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route exact path='/'>
-          <Layout showHome />
-        </Route>
-        <Route path='/dashboards'>
-          <Layout showDashboard />
-        </Route>
-        <Route path='*'>
-          <NoMatch />
-        </Route>
+        <Route path="/" element={<Layout showHome={true} />} />
+        <Route path="/dashboards" element={<Layout showDashboard={true} />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   )
